bdc6b8538d4368466de671ff1f0bf8999c46e909
Greatdev655 <greatiyke7@gmail.com>
2026-01-21 17:55:07 +0100

    wrote and completed the fraction program applying encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        // creating constructors 
+
+        Fraction fractions1 = new Fraction();
+        Console.WriteLine(fractions1.GetFractionString());
+        Console.WriteLine(fractions1.GetFractionDecimal());
+
+        Fraction fractions2 = new Fraction(6);
+        Console.WriteLine(fractions2.GetFractionString());
+        Console.WriteLine(fractions2.GetFractionDecimal());
+
+        Fraction fractions3 = new Fraction(6,7);
+        Console.WriteLine(fractions3.GetFractionString());
+        Console.WriteLine(fractions3.GetFractionDecimal());
+
+        Fraction fraction4 = new Fraction(7,6);
+        Console.WriteLine(fractions3.GetFractionString());
+        Console.WriteLine(fractions3.GetFractionDecimal());
+
+
+        
+
+    
+
+
+
     }
 }
